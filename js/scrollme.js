dba8f022933f8fdc94202d430e5f806859daa28d
Vanessa Kist Silva <vanesasilva_sc@hotmail.com>
2018-05-06 17:36:31 -0300

    First website upload

---
+++ js/scrollme.js
@@ -0,0 +1,51 @@
+(function($){
+	$(document).ready(function(){
+		$(".navscroll").hide();
+
+		$(function(){
+			$(window).scroll(function(){
+				if ($(this).scrollTop()> 600) {
+					$('.navscroll').fadeIn();
+					
+				}else{
+					$('.navscroll').fadeOut();
+
+				}
+			});
+		});
+	});
+}(jQuery));
+
+$(document).ready(function(){
+
+var scrollLink = $('.scroll');
+  
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+
+
+
+// Active link switching
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+    
+    scrollLink.each(function() {
+      
+      var sectionOffset = $(this.hash).offset().top -160;
+      
+      if ( sectionOffset <= scrollbarLocation ) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
+    });
+    
+  });
+  
+});
+
+
